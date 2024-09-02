abf40f5bcd58ff9fa2c8b72c46d1e70ee99658f4
Marcelo Figueiredo <159584958+celofgd@users.noreply.github.com>
2024-09-02 00:39:57 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const botaoPython = document.getElementById("button-python")
+
+botaoPython.addEventListener("click", function() {
+    window.open("https://colab.research.google.com/drive/1Y3ayXJdsO3Fd6sndRMMAGS0HhIeTUex8?usp=sharing", '_blank');
+})
